8cd4eab13620cda6487dda770d8f959d680e7468
Namitha <namithas61@gmail.com>
2020-10-06 12:05:57 +0200

    First Commit

---
+++ CSS/Project2/JAVASCRIPT/new.js
@@ -0,0 +1,57 @@
+javaScript
+
+
+//Data Types
+
+//Strings. Example:"Hello", "I", "1"
+
+//Numbers: Example: 1, 100, 0.5
+
+//Bool: true, false
+
+//x +1 = 3
+ // where x=2
+
+
+//if statements
+
+if (1 + 1  ==2){
+       console.log("That is correct")
+
+}
+
+if (1 + 1  == 3) {
+       console.log("That is correct")
+
+}else{       
+       console.log("You are wrong")
+
+ }
+
+ if (1 + 1  == 3) {
+       console.log("That is correct 1")
+}else if() {       
+       console.log("You are correct 2")
+ }else {
+     console.log("You are wrong")
+ }
+
+ // while loops
+ 
+ while(1 < 100) {
+ 
+    console.log(i)
+
+ }
+
+// for loops
+
+for( var i = 0; i < 10; i++){
+   console.log(i)  ; 
+}
+
+// Functions
+
+function sayHello (word) {
+       console.log(word);
+}
